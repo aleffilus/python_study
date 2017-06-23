7e56f8589ad399989f2ebd538cba4e8f72de9c0d
Alessandro Oliveira <alessandro.fillus@gmail.com>
2017-06-22 23:48:27 -0300

    primeiro commit

---
+++ function.py
@@ -0,0 +1,8 @@
+def hellow_word():
+    print('Hello Word Python')
+
+def hellow_word_name(name):
+    print('Hello Word Python', name)
+
+hellow_word()
+hellow_word_name('Alessandro')
---
+++ while.py
@@ -0,0 +1,5 @@
+contador = 0
+while (contador < 50):
+    print ('the count is: ', contador)
+    contador = contador + 1
+print('Bye')
